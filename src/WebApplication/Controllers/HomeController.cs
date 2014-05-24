f1380a42e9b0ef20894339c9ff84dadb6fb92eab
Tomas Jansson <tomas.jansson@gmail.com>
2014-05-24 17:24:49 +0200

    added web project and layout

---
+++ src/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Code = @"
+using System;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(""Hello World!"");
+    }
+}";
+
+            return View();
+        }
+
+        public ActionResult Compile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
