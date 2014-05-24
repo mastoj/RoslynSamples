2e54068de811b23b88ff7afc6bdeda8b39461743
Tomas Jansson <tomas.jansson@gmail.com>
2014-05-24 16:28:59 +0200

    Added skeleton for the console

---
+++ src/RoslynSamples.Console/CodeExecutor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoslynSamples.Console
+{
+    public class CodeExecutor
+    {
+        public void CompileAndExecute(string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/RoslynSamples.Console/Program.cs
+++ src/RoslynSamples.Console/Program.cs
@@ -10,6 +10,30 @@ namespace RoslynSamples.Console
     {
         static void Main(string[] args)
         {
+            var code = @"
+using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(""Hello {0}"", args[0]);
+    }
+}";
+            System.Console.WriteLine("Executing code: ");
+            System.Console.WriteLine(code);
+            System.Console.WriteLine("With argument:  NNUG");
+            var codeExecutor = new CodeExecutor();
+            codeExecutor.CompileAndExecute(code);
+            System.Console.ReadLine();
+        }
+    }
+
+    public class CodeExecutor
+    {
+        public void CompileAndExecute(string code)
+        {
+            throw new NotImplementedException();
         }
     }
 }
